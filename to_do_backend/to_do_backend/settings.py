275173135245622304f800427669a7e1c8fe21bb
Ashifaachi <ashifaachi300@gmail.com>
2025-04-16 12:46:25 +0530

    fifth commit

--- to_do_backend/to_do_backend/settings.py
+++ to_do_backend/to_do_backend/settings.py
@@ -59,13 +59,17 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5175",  # React/Vite frontend
     "http://127.0.0.1:5175",
+    "http://localhost:5173", # React/Vite frontend
+    "http://127.0.0.1:5173",
 ]
 
 CORS_ALLOW_CREDENTIALS = True  # Allows cookies, authorization headers
 CORS_ALLOW_HEADERS = ["*"]  # Allow all headers
 
 CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
     "http://localhost:5175",
+    "http://127.0.0.1:5173",
     "http://127.0.0.1:5175",
 ]
 
